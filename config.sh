50a9ccf1a6e85264414749172d4e1e673c8cb09f
Mar Yvan Sagun <gagokayvan@gmail.com>
2019-12-14 21:32:36 +0800

    config/installer : Argon update

---
+++ config.sh
@@ -0,0 +1,30 @@
+AUTOMOUNT=false
+PROPFILE=true
+POSTFSDATA=false
+LATESTARTSERVICE=false
+#MINAPI=21
+#MAXAPI=25
+
+print_modname() {
+  ui_print "|================================|"
+  ui_print "|    _____ ____ __ _____ _____   |" 
+  ui_print "|   |  _  | __ || |  _  |   | |  |"
+  ui_print "|   | |_| |   -|| | |_| | | | |  |" 
+  ui_print "|   |_____|_|__|__|_____|_|___|  |"
+  ui_print "|     ~by Shiroyuki82 © 2019     |"
+  ui_print "|                                |"
+  ui_print "|================================|"
+  ui_print ""
+  ui_print "|================================|"
+  ui_print "| Version  : v3.0                |"
+  ui_print "| Codename : Argon               |"
+  ui_print "| Released Date : 12/09/2019     |"
+  ui_print "| Status : Stable                |"
+  ui_print "|================================|"
+}
+
+REPLACE=""
+
+set_permissions() {
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+}
